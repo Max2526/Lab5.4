e202f4f73d75c606a57ee5707f37107a84996688
username <skulamaksym@gmail.com>
2022-05-26 18:25:42 +0300

    lab

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Батько: Скула Василь Володимирович, 1975 року народження, підприємець");
+        System.out.println("Мати: Скула Оксана Миколаївна, 1974 року народження, безробітня");
+        System.out.println("Сестра: Скула Людмила Василівна, 2006 року народження, школяр");
+    }
+}
